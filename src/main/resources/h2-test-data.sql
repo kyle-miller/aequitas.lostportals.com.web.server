87aafe931d670d685d5542645a69ac58292532ba
kyle-miller <frgttnsin@gmail.com>
2016-05-05 15:19:20 -0500

    Adding more test data

--- src/main/resources/h2-test-data.sql
+++ src/main/resources/h2-test-data.sql
@@ -1,3 +1,58 @@
+INSERT INTO entityTypes VALUES ('28697fdd-5a92-4ab1-ab59-cacd173246ab', NULL, 'Boss', TRUE);
+INSERT INTO entityTypes VALUES ('af92dc64-ab4c-4639-bdfd-2dd7fe0c5475', NULL, 'Trees', TRUE);
+INSERT INTO entityTypes VALUES ('9830dd00-2c43-415e-97b1-fc9e2fd542e5', 'af92dc64-ab4c-4639-bdfd-2dd7fe0c5475', 'Ash', TRUE);
+INSERT INTO entityTypes VALUES ('a5edda1c-6b00-4bfc-8941-329ef6d20a9b', 'af92dc64-ab4c-4639-bdfd-2dd7fe0c5475', 'Birch', TRUE);
+INSERT INTO entityTypes VALUES ('68900301-6034-4a33-baad-bdd0458b5862', 'af92dc64-ab4c-4639-bdfd-2dd7fe0c5475', 'Maple', TRUE);
+INSERT INTO entityTypes VALUES ('6312c8f1-26eb-4e0c-bcfe-d628c58fa6f5', 'af92dc64-ab4c-4639-bdfd-2dd7fe0c5475', 'Pine', TRUE);
+INSERT INTO entityTypes VALUES ('fdd4ee35-2e6d-41cc-98f9-67636483c294', NULL, 'Horse Spawn', TRUE);
+INSERT INTO entityTypes VALUES ('56add563-2e0a-4ee1-b650-2b034ebb6f5a', NULL, 'Fishing Spot', TRUE);
+INSERT INTO entityTypes VALUES ('9bf0cd7a-a1ef-4cf2-8adb-abf991a38c47', NULL, 'Treasure Chest', TRUE);
+INSERT INTO entityTypes VALUES ('27e89912-19f6-43d5-a5d6-ff840200a533', NULL, 'Rainbow Node', TRUE);
+INSERT INTO entityTypes VALUES ('d7f97cb8-c47c-45bb-9dff-4bedbb823657', NULL, 'Village', TRUE);
+INSERT INTO entityTypes VALUES ('5e20d6b7-0c8a-4035-9309-38082131fddf', NULL, 'Farm', TRUE);
+INSERT INTO entityTypes VALUES ('beac6540-d6f2-4c32-b345-ffbda51bea70', NULL, 'Town', TRUE);
+INSERT INTO entityTypes VALUES ('e8129d0f-63c4-4f4a-9585-3a0221f81af6', NULL, 'Danger', TRUE);
+INSERT INTO entityTypes VALUES ('91ebb24e-33f1-419d-8106-9d5ee4bebc7e', NULL, 'Connect', TRUE);
+INSERT INTO entityTypes VALUES ('065523ec-f1b9-4103-bd9e-28ddb74f0ebb', NULL, 'Trade', TRUE);
+
+
+
+INSERT INTO entities VALUES ('96158745-6847-440a-8dbe-b169ed2eb18f', 'Altinova');
+INSERT INTO entities VALUES ('de44ff9a-14d0-4432-a6f7-0025520bda72', 'Behr');
+INSERT INTO entities VALUES ('61c05880-1f25-4485-b05b-31cbf12a0bf2', 'Calpheon');
+INSERT INTO entities VALUES ('2b229931-10e6-4824-a7c7-a2f74571ad1c', 'Epheria Port');
+INSERT INTO entities VALUES ('081488ad-86ac-43fc-9d15-843b5b6ac133', 'Florin');
+INSERT INTO entities VALUES ('f0f7ba28-a0d5-420a-a06b-5414524f36ca', 'Glish');
+INSERT INTO entities VALUES ('fb3729cb-9fd9-40c7-9044-a3325374c217', 'Heidel');
+INSERT INTO entities VALUES ('3f3c0970-0023-4559-9502-177e6209c7a1', 'Iliya');
+INSERT INTO entities VALUES ('4e705587-d08f-41d5-8c03-47559e92bc5a', 'Kelphan');
+INSERT INTO entities VALUES ('b20b6233-2582-47ad-802c-fd190e011437', 'Northern Wheat Plantation');
+INSERT INTO entities VALUES ('4fc9c713-62ec-47fe-ac01-962143f8342c', 'Olvia');
+INSERT INTO entities VALUES ('c3f91eb8-cd2e-4c95-8006-af15bac329cf', 'Tarif');
+INSERT INTO entities VALUES ('4bf3e60a-15ae-4fc6-bf0d-cd70778449f6', 'Trent');
+INSERT INTO entities VALUES ('43ff5b8a-dcb6-4eb1-9c5f-719cf28e0b61', 'Velia');
+INSERT INTO entities VALUES ('4fe8629a-f45a-4e21-9e1e-244c2c8a6213', 'Western Guard Camp');
+
+
+
+INSERT INTO entityEntityTypeXrefs VALUES ('cbd9cea7-b1ca-4b75-bb7b-8c48c1d34f2f', '96158745-6847-440a-8dbe-b169ed2eb18f', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('c20b8a00-1433-49ef-b57a-489f4272b201', 'de44ff9a-14d0-4432-a6f7-0025520bda72', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('2878b036-23e7-4367-be25-9207e4f901a1', '61c05880-1f25-4485-b05b-31cbf12a0bf2', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('7e63afe8-28e1-4524-80ca-16c52ea898c8', '2b229931-10e6-4824-a7c7-a2f74571ad1c', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('6c4a57d1-a68a-44f8-9c91-8e0fc46db82d', '081488ad-86ac-43fc-9d15-843b5b6ac133', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('3fab46ef-5d4b-411b-a286-05310d67b316', 'f0f7ba28-a0d5-420a-a06b-5414524f36ca', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('87570019-d368-45ff-abbe-2eb69f2add5f', 'fb3729cb-9fd9-40c7-9044-a3325374c217', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('abc6b600-2da3-4ce6-91af-30e668ea111a', '3f3c0970-0023-4559-9502-177e6209c7a1', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('0c2fe00b-309a-4868-bc00-7e87999bf5dc', '4e705587-d08f-41d5-8c03-47559e92bc5a', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('d7acf07d-235e-4c33-a058-96dcf926fabe', 'b20b6233-2582-47ad-802c-fd190e011437', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('c17760ab-f130-4bb6-bf30-00569a6020d3', '4fc9c713-62ec-47fe-ac01-962143f8342c', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('ea8e1045-906b-458e-97e0-624b85928806', 'c3f91eb8-cd2e-4c95-8006-af15bac329cf', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('8bd0ff63-cb44-4fa3-8994-39cad703e49f', '4bf3e60a-15ae-4fc6-bf0d-cd70778449f6', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('55547c69-2fe5-4177-abf9-7513f8330102', '43ff5b8a-dcb6-4eb1-9c5f-719cf28e0b61', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+INSERT INTO entityEntityTypeXrefs VALUES ('171b587b-be6d-4a1a-987e-a3d0d865b876', '4fe8629a-f45a-4e21-9e1e-244c2c8a6213', 'beac6540-d6f2-4c32-b345-ffbda51bea70');
+
+
+
 INSERT INTO circles VALUES ('3ac9fb5f-7efa-481a-8c2f-dc0b525fd082', '9ee3f882-29c1-4c3b-9e59-5cc79dda8ff4', -45.12345678, -33.98765432, 5, '#50BB34', 'red');
 INSERT INTO entities VALUES ('9ee3f882-29c1-4c3b-9e59-5cc79dda8ff4', 'A Circle');
 INSERT INTO entities VALUES ('8f616015-fd36-4f7b-bfa4-44a255272b20', 'A Marker');
@@ -5,9 +60,7 @@ INSERT INTO entities VALUES ('56b007a7-8b24-4141-8d80-7f8b3b269a4a', 'A Polygon'
 INSERT INTO entityEntityTypeXrefs VALUES ('7c9e13df-ca2a-49ef-a787-e6573521b9d9', '9ee3f882-29c1-4c3b-9e59-5cc79dda8ff4', '28697fdd-5a92-4ab1-ab59-cacd173246ab');
 INSERT INTO entityEntityTypeXrefs VALUES ('4f28e862-0aa0-4842-b6be-6a8ad1334c57', '8f616015-fd36-4f7b-bfa4-44a255272b20', 'af92dc64-ab4c-4639-bdfd-2dd7fe0c5475');
 INSERT INTO entityEntityTypeXrefs VALUES ('649cf5dd-c6ff-48bf-a7ed-6e454dd9ddb1', '56b007a7-8b24-4141-8d80-7f8b3b269a4a', 'fdd4ee35-2e6d-41cc-98f9-67636483c294');
-INSERT INTO entityTypes VALUES ('28697fdd-5a92-4ab1-ab59-cacd173246ab', NULL, 'Boss', TRUE);
-INSERT INTO entityTypes VALUES ('af92dc64-ab4c-4639-bdfd-2dd7fe0c5475', NULL, 'Trees', TRUE);
-INSERT INTO entityTypes VALUES ('fdd4ee35-2e6d-41cc-98f9-67636483c294', NULL, 'Horse Spawn', TRUE);
+
 INSERT INTO icons VALUES ('38908f09-d5be-4968-a33e-f4ef2d431a62', 'http://aequitas.lostportals.com/images/icon.png', 'Treasure');
 INSERT INTO markers VALUES ('fb6985ff-08d8-44a0-829a-11b7d64e3b7f', '8f616015-fd36-4f7b-bfa4-44a255272b20', -30.12345678, -30.98765432, '38908f09-d5be-4968-a33e-f4ef2d431a62');
 INSERT INTO notes VALUES ('90997e68-d596-4e84-82b6-fcf39fdac9c3', '9ee3f882-29c1-4c3b-9e59-5cc79dda8ff4', 'A circle note', 1);
