bd8098aa0ba97333f148e7278dc77d93d09a03f5
Kyle Miller <frgttnsin@gmail.com>
2016-05-08 20:15:32 -0500

    First attempt at postgre

--- src/main/java/com/lostportals/aequitas/Application.java
+++ src/main/java/com/lostportals/aequitas/Application.java
@@ -5,19 +5,20 @@ import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
 	public DataSource getDataSource() {
-		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		builder.setType(EmbeddedDatabaseType.H2).addScripts("h2-schema.sql", "h2-test-data.sql");
-		return builder.build();
+		DriverManagerDataSource dmds = new DriverManagerDataSource("jdbc:postgresql://localhost:5432/aequitas/", "postgre", "password");
+//		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+//		builder.setType(EmbeddedDatabaseType.H2).addScripts("h2-schema.sql", "h2-test-data.sql");
+		return dmds;
 	}
 }
